b269af7de669ea07c4fb93210f8b778ead93a9a8
Dmytro Martynenko <dmitrom89@gmail.com>
2017-08-26 18:59:36 +0300

    language-team finish front-end

---
+++ js/main.js
@@ -0,0 +1,97 @@
+/**
+ * Created by maddem on 17.08.2017.
+ */
+$(document).ready(function(){
+
+	/**
+	 * Инициализация слайдера
+	 */
+	$('.main-slider').slick({
+		speed: 1000,
+		slidesToShow: 3,
+		centerMode: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+				}
+			},
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+				}
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: false
+				}
+			},
+			{
+				breakpoint: 543,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: false,
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+
+	/**
+	 * Плавная прокрутка к якорю
+	 */
+	$(".navbar-header").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href');
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		var top = $(id).offset().top;
+		top = top - top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+
+	/**
+	 * Инициализация блока акции
+	 */
+	function specialOffer() {
+		$('.special-offer').modal('show')
+	}
+	setTimeout(specialOffer, 5000);
+});
